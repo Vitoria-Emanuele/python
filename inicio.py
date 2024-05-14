95d85da44c1098de382ef31d13be0792342e486e
Vitoria <vitoriaemanueledesousa@gmail.com>
2024-05-14 11:25:18 -0300

    commit

---
+++ inicio.py
@@ -0,0 +1,12 @@
+# python nao eh uma linguagem fortemente tipada, por esse motivo nao precisamos inicializar e nem se quer chamar bibçiotecas
+
+x = 10
+y = 2
+print(x+y)
+
+#um programa simples que definiu duas variaveis, sem precisar inicializa-las, com seus valores e exibiu a soma dessas duas variaveis
+
+z = input(int("%d"))
+t = input(float("%d"))
+zt = z+t
+print(zt)
